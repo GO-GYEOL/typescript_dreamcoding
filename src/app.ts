836d7e9b773190b771b8a2e92342192a388c05e7
고결 <rhruf3297@gmail.com>
2022-10-21 18:05:34 +0900

    feat: note.ts, todo.ts, video.ts

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,6 @@
+import { VideoComponent } from "./components/page/item/video.js";
+import { ToDoComponent } from "./components/page/item/todo.js";
+import { NoteComponent } from "./components/page/item/note.js";
 import { ImageComponent } from "./components/page/item/image.js";
 import { PageComponent } from "./components/page/page.js";
 class App {
@@ -12,6 +15,18 @@ class App {
       "http://picsum.photos/600/300"
     );
     image.attachTo(appRoot, "beforeend");
+
+    const video = new VideoComponent(
+      "Video Title",
+      "https://youtu.be/tzIzcr4EM6w"
+    );
+    video.attachTo(appRoot, "beforeend");
+
+    const note = new NoteComponent("Note Title", "Note Body");
+    note.attachTo(appRoot, "beforeend");
+
+    const todo = new ToDoComponent("Todo Title", "Todo Item");
+    todo.attachTo(appRoot, "beforeend");
   }
 }
 
---
+++ src/components/page/item/video.ts
@@ -0,0 +1,33 @@
+import { BaseComponent } from "./../../component.js";
+export class VideoComponent extends BaseComponent<HTMLElement> {
+  constructor(title: string, url: string) {
+    super(`
+                <section class="video">
+                    <div class="video__player">
+                        <iframe class="video__iframe"></iframe>
+                    </div>
+                    <h3 class="video__title"></h3>
+                </section>`);
+
+    const iframe = this.element.querySelector(
+      ".video__iframe"
+    )! as HTMLIFrameElement;
+    // iframe.src = "https://www.youtube.com/embed/tzIzcr4EM6w";
+    iframe.src = this.convertToEmbeddedURL(url);
+
+    const titleElement = this.element.querySelector(
+      ".video__title"
+    )! as HTMLHeadingElement;
+    titleElement.textContent = title;
+  }
+  private convertToEmbeddedURL(url: string): string {
+    const regExp =
+      /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+    const match = url.match(regExp);
+    const videoId = match ? match[1] || match[2] : undefined;
+    if (videoId) {
+      return `https://www.youtube.com/embed/${videoId}`;
+    }
+    return url; // 매칭값이 없을 경우. 아니면 undefined를 리턴하고, 위에서 함수를 호출한 부분에 undefined가 나온다면 사용자에게 경고 팝업을 보내는 등 다양한 방식으로 해볼 수 있다.
+  }
+}
