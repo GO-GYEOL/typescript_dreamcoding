5d91cc5a1a940398662163cd4a5824fea87891a9
고결 <rhruf3297@gmail.com>
2022-10-26 15:44:45 +0900

    refactoring: PageComponent coupling is lowered

--- src/app.ts
+++ src/app.ts
@@ -2,40 +2,35 @@ import { VideoComponent } from "./components/page/item/video.js";
 import { ToDoComponent } from "./components/page/item/todo.js";
 import { NoteComponent } from "./components/page/item/note.js";
 import { ImageComponent } from "./components/page/item/image.js";
-import { Composable, PageComponent } from "./components/page/page.js";
+import {
+  Composable,
+  PageComponent,
+  PageItemComponent,
+} from "./components/page/page.js";
 import { Component } from "./components/component.js";
 class App {
-  // private readonly page: PageComponent;
   private readonly page: Component & Composable;
-  // page는 Component 이면서 addChild가 가능한 Composable요소이다.
-  // 생성자 안에서 page 컴포넌트를 만들었다. 사실 생성자 내부에서 다른 클래스를 만드는 것은 사실 조금 위험하므로 DI를 이용해 외부로부터 주입을 받는 것이 더 확장성도 좋고 유닛테스트도 편리하다고 한다.
-  // 우선은 이렇게 둔다.
   constructor(appRoot: HTMLElement) {
-    this.page = new PageComponent();
+    this.page = new PageComponent(PageItemComponent);
     this.page.attachTo(appRoot);
 
     const image = new ImageComponent(
       "Image Title",
       "http://picsum.photos/600/300"
     );
-    // image.attachTo(appRoot, "beforeend");
     this.page.addChild(image);
-    
 
     const video = new VideoComponent(
       "Video Title",
       "https://youtu.be/tzIzcr4EM6w"
     );
-    // video.attachTo(appRoot, "beforeend");
     this.page.addChild(video);
 
     const note = new NoteComponent("Note Title", "Note Body");
     this.page.addChild(note);
-    // note.attachTo(appRoot, "beforeend");
 
     const todo = new ToDoComponent("Todo Title", "Todo Item");
     this.page.addChild(todo);
-    // todo.attachTo(appRoot, "beforeend");
   }
 }
 
--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -4,15 +4,19 @@ export interface Composable {
   addChild(child: Component): void;
 }
 
-// 타입 정의해줌
 type OnCloseListener = () => void;
 
-class PageItemComponent
-  extends BaseComponent<HTMLElement>
-  implements Composable
+interface SectionContainer extends Component, Composable {
+  setOnCloseListener(listener: OnCloseListener): void;
+}
+
+type SectionContainerConstructor = {
+  new (): SectionContainer; 
+}
+
+export class PageItemComponent extends BaseComponent<HTMLElement> implements SectionContainer
 {
-  private closeListener? : OnCloseListener
-  // 외부로부터 전달받을 콜백함수를 저장하고 있을 변수를 만들었다.
+  private closeListener?: OnCloseListener;
   constructor() {
     super(`<li class="page-item">
                 <section class="page-item__body"></section>
@@ -23,11 +27,8 @@ class PageItemComponent
   `);
     const closeBtn = this.element.querySelector(".close")! as HTMLButtonElement;
     closeBtn.onclick = () => {
-      // this.element.remove() 해도 되지만, (실제로 지금은 이 방법이 맞음)
-      // 모든 pageItemComponent 들은 PageItem 내부에 존재하고 있다. 데이터는 직접 자신이 삭제하기 보다는, 전체적으로 관리 하고 있는 대상에서 위임하는것이 더 안전한 코딩 방법이다. 
-      // 그래서 존재하는 모든 페이지 아이템들에 대해 관리해야하는 경우, 페이지 아이템이 삭제가 되는 장소인 페이지가 삭제 로직을 가지고 처리해주는 것이 좋다고 함.
       this.closeListener && this.closeListener();
-    }
+    };
   }
   addChild(child: Component) {
     const container = this.element.querySelector(
@@ -35,7 +36,7 @@ class PageItemComponent
     )! as HTMLElement;
     child.attachTo(container);
   }
-  setOnCloseListener(listener : OnCloseListener){
+  setOnCloseListener(listener: OnCloseListener) {
     this.closeListener = listener;
   }
 }
@@ -44,15 +45,15 @@ export class PageComponent
   extends BaseComponent<HTMLUListElement>
   implements Composable
 {
-  constructor() {
+  constructor(private pageItemConstructor: SectionContainerConstructor) {
     super('<ul class="page">asd</ul>');
   }
   addChild(section: Component) {
-    const item = new PageItemComponent();
+    const item = new this.pageItemConstructor();
     item.addChild(section);
     item.attachTo(this.element, "beforeend");
-    item.setOnCloseListener(()=> {
+    item.setOnCloseListener(() => {
       item.removeFrom(this.element);
-    })
+    });
   }
 }
