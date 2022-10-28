818fdb4d2689727c79d5cf39b5b8d918f5b7a33f
고결 <rhruf3297@gmail.com>
2022-10-28 14:40:28 +0900

    css styling

--- src/components/page/item/image.ts
+++ src/components/page/item/image.ts
@@ -1,13 +1,13 @@
 import { BaseComponent } from "./../../component.js";
 export class ImageComponent extends BaseComponent<HTMLElement> {
   constructor(title: string, url: string) {
-    super(`
-    <section class="image">
+    super(`<section class="image">
     <div class="image__holder">
-      <img class="image__thumbnail" />
-      <h2 class="image__title"></h2>
+    <img class="image__thumbnail">
     </div>
-  </section>;`);
+    <h2 class="page-item__title image__title"></h2>
+  </section>`);
+  
     const imageElement = this.element.querySelector(
       ".image__thumbnail"
     )! as HTMLImageElement;
--- src/components/page/item/todo.ts
+++ src/components/page/item/todo.ts
@@ -2,8 +2,9 @@ import { BaseComponent } from "./../../component.js";
 export class ToDoComponent extends BaseComponent<HTMLElement> {
   constructor(title: string, todo: string) {
     super(`<section class="todo">
-                <h2 class="todo__title"></h2>
-                <input type="checkbox" class="todo-checkbox">
+                <h2 class="page-item__title todo__title"></h2>
+                <input type="checkbox" id="todo-checkbox">
+                <label for="todo-checkbox" class="todo-label"></label>
             </section>`);
     const titleElement = this.element.querySelector(
       ".todo__title"
@@ -11,8 +12,8 @@ export class ToDoComponent extends BaseComponent<HTMLElement> {
     titleElement.textContent = title;
 
     const todoElement = this.element.querySelector(
-      ".todo-checkbox"
-    )! as HTMLInputElement;
-    todoElement.insertAdjacentText("afterend", todo);
+      ".todo-label"
+    )! as HTMLLabelElement;
+    todoElement.textContent = todo;
   }
 }
